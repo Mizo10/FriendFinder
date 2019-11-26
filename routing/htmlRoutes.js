7b663514d82461453dd183e02fc96660735c8e41
Mizo10 <moutazabdelhamid@gmail.com>
2019-11-25 20:05:08 -0500

    fixing app crash bug

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     });
 
     //If not routing is found
-    app.get("*", function(req, res){
+    app.get("/", function(req, res){
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 }
